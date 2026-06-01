1e870206027934ab8efbcc3c401aed1333adcf5e
yogirajmagar07 <99736643+yogirajmagar07@users.noreply.github.com>
2026-06-01 17:03:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,11 @@ def logout():
 @login_required
 def dashboard():
     return render_template("index.html")
-
+# Dashboard Tauras 24
+@app.route('/tauras_dashboard')
+@login_required
+def dashboard():
+    return render_template("tauras.html")
 
 # =========================
 # REPORT PAGE
